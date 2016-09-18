c891f8504643620b39aa8c2f2bf0aa83b3371274
jiashasha <jiashasha26@163.com>
2016-09-18 18:48:37 +0800

    Add files via upload

---
+++ js/travel.js
@@ -0,0 +1,20 @@
+$(function(){
+	$("[data-toggle='tooltip']").tooltip();
+});
+
+// 鼠标悬停下划线动态出现
+// $(function(){
+// 	$('.nav li').hover(function(){
+// 		$('span',this).stop().css('height','2px');
+// 		$('span',this).animate({
+// 			left:'0',
+// 			width:'100%',
+// 			right:'0'
+// 		},200);
+// 	},function(){
+// 		$('span',this).stop().animate({
+// 			left:'50%',
+// 			width:'0'
+// 		},200);
+// 	});
+// });
